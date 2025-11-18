2e5fb39c43a2d1de6de3b8e32abb8a4fe7a7da3c
Levis <leviskapkory@gmail.com>
2025-11-18 23:27:58 +0300

    fixed issue with registration and login

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Include auth routes (login, register, etc.)
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+   return $request->user()->only(['uuid', 'name', 'email']);
 });
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    dd(\Illuminate\Support\Facades\Hash::make('leviskapkory@gmail.com'));
     return ['Laravel' => app()->version()];
 });
 
