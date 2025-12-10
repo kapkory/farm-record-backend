b08ecf568ee7bd2b4de25835f4c4c1a762139f26
Levis <leviskapkory@gmail.com>
2025-12-10 11:58:49 +0300

    clear workspace

---
+++ app/Traits/ApiResponse.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Traits;
+
+trait ApiResponse {
+    protected function successResponse($data, $message = "Request was successful", int $code = 200): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+    protected function errorResponse(string $message, int $code = 400, ?array $errors = null): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'status' => 'error',
+            'message' => $message,
+            'errors' => $errors,
+        ], $code);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -26,3 +27,4 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
